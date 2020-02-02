cc1bae90c94c989d87edd07d99a64e3b82a49d19
LisbethOrtogorin <130348@unsaac.edu.pe>
2020-02-02 10:51:49 -0500

    primer componente

--- ecotoner-app/src/app/app.module.ts
+++ ecotoner-app/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './components/app/app-routing.module';
+import { AppComponent } from './components/app/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { UbicacionComponent } from './components/ubicacion/ubicacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NosotrosComponent,
+    UbicacionComponent
   ],
   imports: [
     BrowserModule,
