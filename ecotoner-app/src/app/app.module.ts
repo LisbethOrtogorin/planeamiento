b3c6f933907b1905e6f0976ee881767ba3e6e0d9
LisbethOrtogorin <130348@unsaac.edu.pe>
2020-02-02 19:49:40 -0500

    mas componentes y rutas

--- ecotoner-app/src/app/app.module.ts
+++ ecotoner-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './components/app/app-routing.module';
+
 import { AppComponent } from './components/app/app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { UbicacionComponent } from './components/ubicacion/ubicacion.component';
+import { ProductoComponent } from './components/producto/producto.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { UbicacionComponent } from './components/ubicacion/ubicacion.component';
     HeaderComponent,
     FooterComponent,
     NosotrosComponent,
-    UbicacionComponent
+    UbicacionComponent,
+    ProductoComponent
   ],
   imports: [
     BrowserModule,
--- ecotoner-app/src/app/components/app/app-routing.module.ts
+++ ecotoner-app/src/app/components/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NosotrosComponent } from '../nosotros/nosotros.component';
+import { UbicacionComponent } from '../ubicacion/ubicacion.component';
+import { ProductoComponent } from '../producto/producto.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProductoComponent},
+  {path:'nosotros', component:NosotrosComponent},
+  {path: 'plan-estrategico', component:UbicacionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
